e3f91a639b39fc53012f2a739dc754338da65b43
twknab <20636750+twknab@users.noreply.github.com>
2019-01-08 18:17:19 -0800

    added front end validations for all 3 forms, set global methods to update logged in user per login/registration/profile update. going to add in a router now and restructure the app with webpack and being able to handle redirects etc. users are currently stored in an array that will hold data as so long as SPA is not refreshed (single page app, once refreshed, will invoke a new empty registered users array -- because there is no backend, there is no long-term storage)

--- main.js
+++ main.js
@@ -39,35 +39,16 @@ Vue.component('register', {
   methods: {
     registerUser() {
       // begin validation:
+
       // empty errors array if any existing errors:
       this.errors = [];
 
       // validate submission (front-end validations only):
-      // first and last name required and at least 2 characters:
-      if (!this.firstName || !this.lastName) {
-        this.errors.push("First and last name are both required.");
-      } else if (this.firstName.length < 2 || this.lastName.length < 2) {
-        this.errors.push("First and last name must be at least 2 characters.");
-      }
-      // email and password required, password at least 6 characters:
-      if (!this.email || !this.password) {
-        this.errors.push("Email and password are both required.");
-      } else if (this.password.length < 6) {
-        this.errors.push("Password must be at least 8 characters");
-      }
-      // phone number required, at least 10 characters:
-      if (!this.phoneNumber) {
-        this.errors.push("Phone number is required.");
-      } else if (this.phoneNumber.length < 10) {
-        this.errors.push("Phone number must be at least 10 characters");
-      }
-      // email must be unique:
+      this.errors = this.validateRegistration(this);
 
       // if validation passes:
-      else {
-        console.log("-------");
-        console.log(this);
-        console.log("-------");
+      if (this.errors.length === 0) {
+
         // capture new user details:
         let newUser = {
           firstName: this.firstName,
@@ -76,17 +57,43 @@ Vue.component('register', {
           password: this.password,
           phoneNumber: this.phoneNumber,
         };
-        console.log(newUser);
+
         // reset values to null after form submission:
         this.firstName = null;
         this.lastName = null;
         this.email = null;
         this.password = null;
         this.phoneNumber = null;
-        // emit to global function and send new User:
+
+        // emit to global function and send new User to update global scope:
         this.$emit('register-user', newUser);
       }
     },
+    validateRegistration(user) {
+      // create array to store errors:
+      let errors = [];
+      // first and last name required and at least 2 characters:
+      if (!user.firstName || !user.lastName) {
+        errors.push("First and last name are both required.");
+      } else if (user.firstName.length < 2 || user.lastName.length < 2) {
+        errors.push("First and last name must be at least 2 characters.");
+      }
+      // email and password required, password at least 6 characters:
+      if (!user.email || !user.password) {
+        errors.push("Email and password are both required.");
+      } else if (user.password.length < 6) {
+        errors.push("Password must be at least 6 characters.");
+      } else if (user.email.length < 5) {
+        errors.push("Email must be at least 5 characters.");
+      }
+      // phone number required, at least 10 characters:
+      if (!user.phoneNumber) {
+        errors.push("Phone number is required.");
+      } else if (user.phoneNumber.length < 10) {
+        errors.push("Phone number must be at least 10 characters");
+      }
+      return errors;
+    }
   }
 });
 
@@ -97,6 +104,12 @@ Vue.component('login', {
       <h1>Login</h1>
       <fieldset>
         <form action="" @submit.prevent="loginUser">
+          <p v-if="errors.length">
+            <em>Please fix the following errors:</em>
+            <ul>
+              <li v-for="error in errors">{{ error  }}</li>
+            </ul>
+          </p>
           <input v-model="email" type="email" name="email" id="login_email" placeholder="Email">
           <br/>
           <input v-model="password" type="password" name="password" id="login_password" placeholder="Password">
@@ -115,40 +128,76 @@ Vue.component('login', {
   },
   methods: {
     loginUser() {
-      // capture user details to find:
-      let findUser = {
-        email: this.email,
-        password: this.password,
-      };
-      console.log(findUser);
-      // reset values to null after form submission:
-      this.email = null;
-      this.password = null;
+      // begin validation:
 
-      // emit login-user() method globally and send findUser data object with it:
-      this.$emit('login-user', findUser);
+      // empty errors array if any existing errors:
+      this.errors = [];
+
+      // validate submission (front-end validations only):
+      this.errors = this.validateLogin(this);
+
+      // If validation passes:
+      if (this.errors.length === 0) {
+
+        // capture user details to find:
+        let findUser = {
+          email: this.email,
+          password: this.password,
+        };
+
+        // reset values to null after form submission:
+        this.email = null;
+        this.password = null;
+  
+        // emit login-user() and send findUser data to update global logged in user:
+        this.$emit('login-user', findUser);
+      }
+    },
+    validateLogin(user) {
+      // create array to store errors:
+      let errors = [];
+      // email and password required, password at least 6 characters:
+      if (!user.email || !user.password) {
+        errors.push("Email and password are both required.");
+      } else if (user.password.length < 6) {
+        errors.push("Password must be at least 6 characters.");
+      }
+      return errors;
     }
   }
 });
 
 // User Profile Component:
 Vue.component('profile', {
+  props: {
+    loggedInUser: {
+      type: Object,
+      required: true,
+    }
+  },
   template: `
     <div>
       <fieldset>
-        <legend>{{ firstName }}'s Profile</legend>
+        {{ loggedInUser }}
+        <legend>{{ loggedInUser.firstName }}'s Profile</legend>
         <form action="" @submit.prevent="updateUser">
-          <img v-bind:src="avatarImgUrl" alt="User Avatar">
+          <p v-if="errors.length">
+            <em>Please fix the following errors:</em>
+            <ul>
+              <li v-for="error in errors">{{ error  }}</li>
+            </ul>
+          </p>
+          <img :src="loggedInUser.avatarImgUrl" alt="User Avatar">
           <br/>
-          <input v-model="firstName" type="text" name="firstName" id="firstName">
+          <input v-model="firstName" type="text" name="firstName" id="firstName" :placeholder="loggedInUser.firstName">
           <br/>
-          <input v-model="lastName" type="text" name="lastName" id="lastName">
+          <input v-model="lastName" type="text" name="lastName" id="lastName" :placeholder="loggedInUser.lastName">
           <br/>
-          <input v-model="email" type="text" name="email" id="email">
+          <input v-model="email" type="text" name="email" id="email" :placeholder="loggedInUser.email">
           <br/>
-          <input v-model="phoneNumber" type="tel" name="phone" id="phone">
+          <input v-model="phoneNumber" type="tel" name="phone" id="phone" :placeholder="loggedInUser.phoneNumber">
           <br/>
-          <input v-model="avatarImgUrl" type="text" name="avatarImgUrl" id="avatarImgUrl">
+          <input v-model="avatarImgUrl" type="text" name="avatarImgUrl" id="avatarImgUrl" :placeholder="loggedInUser.avatarImgUrl">
           <br/>
           <input type="submit" value="Update">
         </form>
@@ -157,24 +206,55 @@ Vue.component('profile', {
   `,
   data() {
     return {
-      firstName: "Tim",
-      lastName: "Knab",
-      email: "tim@knab.com",
-      phoneNumber: "2062711443",
-      avatarImgUrl: "https://via.placeholder.com/150/000000/FFFFFF/?text=UserAvatar",
+      firstName: this.loggedInUser.firstName,
+      lastName: this.loggedInUser.lastName,
+      email: this.loggedInUser.email,
+      phoneNumber: this.loggedInUser.phoneNumber,
+      avatarImgUrl: this.loggedInUser.avatarImgUrl,
       errors: [],
     };
   },
   methods: {
     updateUser() {
-      let updatedUser = {
-        firstName: this.firstName,
-        lastName: this.lastName,
-        email: this.email,
-        phoneNumber: this.phoneNumber,
-        avatarImgUrl: this.avatarImgUrl
-      };
-      this.$emit('update-user', updatedUser);
+      // empty errors array if any existing errors:
+      this.errors = [];
+
+      // validate submission (front-end validations only):
+      this.errors = this.updateUserValidation(this);
+
+      // if validation passes:
+      if (this.errors.length === 0) {
+        let updatedUser = {
+          firstName: this.firstName,
+          lastName: this.lastName,
+          email: this.email,
+          phoneNumber: this.phoneNumber,
+          avatarImgUrl: this.avatarImgUrl
+        };
+        this.$emit('update-user', updatedUser);
+      }
+    },
+    updateUserValidation(user) {
+      let errors = [];
+      // first and last name required and at least 2 characters:
+      if (!user.firstName || !user.lastName) {
+        errors.push("First and last name are both required.");
+      } else if (user.firstName.length < 2 || user.lastName.length < 2) {
+        errors.push("First and last name must be at least 2 characters.");
+      }
+      // email required and at least 5 characters:
+      if (!user.email) {
+        errors.push("Email is required.");
+      } else if (user.email.length < 5) {
+        errors.push("Email must be at least 5 characters.");
+      }
+      // phone number required, at least 10 characters:
+      if (!user.phoneNumber) {
+        errors.push("Phone number is required.");
+      } else if (user.phoneNumber.length < 10) {
+        errors.push("Phone number must be at least 10 characters");
+      }
+      return errors;
     },
     logout() {
       this.$emit('logout-user');
@@ -204,35 +284,74 @@ let app = new Vue({
   el: '#app',
   data: {
     registeredUsers: [],
-    loggedinUser: {}
+    loggedInUser: {
+      firstName: null,
+      lastName: null,
+      email: null,
+      phoneNumber: null,
+      avatarImgUrl: null,
+    }
     // how do we get logged in user for the profile component?
   },
   methods: {
     registerNewUser(newUser) {
       console.log("Registering new user...", newUser);
-      // note: could front-end validate here
+
       // push into array of global data containing new users
       this.registeredUsers.push(newUser);
       console.log(this.registeredUsers);
-      // send our new user to the backend for validation and creation, and wait for a response
+
+      // update logged in user:
+      this.updateLoggedInUser(newUser);
+
       // redirect to profile
+
     },
     loginExistingUser(user) {
       console.log("Logging in existing user...", user);
-      // lookup existing user (we have no db so fake this)
-      // or use a queue of registered users in an array to search within
-      // if user is found store user data
-      // redirect to profile if authenticated, else back home
+
+      // update logged in user:
+      this.updateLoggedInUser(user);
+
+      // redirect to profile
+
     },
     updateUserProfile(updatedUser) {
       console.log("Updating user profile...", updatedUser);
-      // update logged in user data
+      
+      // update logged in user:
+      this.updateLoggedInUser(updatedUser);
+
       // redirect to profile
+
+    },
+    updateLoggedInUser(user) {
+      // update the global scope with new user:
+      this.loggedInUser.firstName = user.firstName;
+      this.loggedInUser.lastName = user.lastName;
+      this.loggedInUser.email = user.email;
+      this.loggedInUser.phoneNumber = user.phoneNumber;
+      if (user.avatarImgUrl.length < 1) {
+        // sets a default avatar image for new users
+        this.loggedInUser.avatarImgUrl = "https://via.placeholder.com/150/000000/FFFFFF/?text=User Avatar";
+      } else {
+        // set avatar to user entered URL
+        this.loggedInUser.avatarImgUrl = user.avatarImgUrl;
+      }
     },
     logoutUser() {
-      console.log("Logging out user...");
-      // remove logged in user from data
+      // Clear logged in user data:
+      this.loggedInUser = {
+        firstName: null,
+        lastName: null,
+        email: null,
+        phoneNumber: null,
+        avatarImgUrl: null,
+      };
+      
       // redirect to homepage
+      // ?????
+      console.log("Logging out user...");
     }
   }
 });
